3229aae9411d1409d3cafdd7a798291559cb542c
Ed Page <eopage@gmail.com>
2026-03-27 16:43:24 -0500

    feat: Add Skeleton cargo subcommand

---
+++ src/bin/cargo-tnew/cli.rs
@@ -0,0 +1,23 @@
+use cargo_tnew::CargoResult;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(bin_name = "cargo")]
+#[command(styles = clap_cargo::style::CLAP_STYLING)]
+pub(crate) enum Command {
+    TNew,
+}
+
+impl Command {
+    pub(crate) fn exec(self) -> CargoResult<()> {
+        match self {
+            Self::TNew => Ok(()),
+        }
+    }
+}
+
+#[test]
+fn verify_app() {
+    use clap::CommandFactory;
+    Command::command().debug_assert();
+}
